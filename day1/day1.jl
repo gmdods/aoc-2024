4f1a8d5a80c59096828133efa1c777f19b6d44a2
gmd01bit <gmd01bit@gmail.com>
2024-12-01 08:01:17 +0200

    :rocket: Day 1

---
+++ day1/day1.jl
@@ -0,0 +1,27 @@
+# using DataStructures
+function counter(a)
+	local d = Dict{Int, Int}()
+	for elt in a; d[elt] = get(d, elt, 0) + 1 end
+	return d
+end
+
+function day1b(f)
+	t = broadcast(readlines(f)) do s
+		s = split(s)
+		(parse(Int, s[1]), parse(Int, s[2]))
+	end
+	t1 = broadcast(p -> p[1], t)
+	t2 = broadcast(p -> p[2], t)
+	c = counter(t2)
+	sum(t1 .* broadcast(n -> get(c, n, 0), t1))
+end
+
+function day1a(f)
+	t = broadcast(readlines(f)) do s
+		s = split(s)
+		(parse(Int, s[1]), parse(Int, s[2]))
+	end
+	t1 = broadcast(p -> p[1], t)
+	t2 = broadcast(p -> p[2], t)
+	sum(abs.(sort!(t1) .- sort!(t2)))
+end
