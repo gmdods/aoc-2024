d8e940eaf0df013fecc12dd6f0527f4bc70c5dfd
gmd01bit <gmd01bit@gmail.com>
2024-12-02 08:23:04 +0200

    :rocket: Day 2

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-all: day1
+all: day1 day2
 
 .PHONY: day1
 day1:
 	julia day1/day1.jl day1/day1.txt
+
+.PHONY: day2
+day2:
+	julia day2/day2.jl day2/day2.txt
---
+++ day2/day2.jl
@@ -0,0 +1,49 @@
+function day2b(f)
+	function safe(di)
+		sg = sign.(di)
+		allequal(sg) && sg[1] != 0 && all(abs.(di) .<= 3)
+	end
+
+	sum(broadcast(readlines(f)) do s
+		t = parse.(Int, split(s))
+		di = diff(t)
+		sg = sign.(di)
+		err0 = (abs.(di) .> 3)
+		err1 = (sg .!= sg[1])
+
+		e0, e1 = sum(err0), sum(err1)
+		if e0 == 0 && e1 == 0
+			return sg[1] != 0
+		elseif e0 <= 1
+			for i = 2:length(di)
+				dn = di[i]
+				# di[i-1] + di[i] = t[i+1] - t[i-1]
+				di[i-1] += dn
+				di[i] = sign(di[i-1])
+				safe(di) && return true
+				di[i] = dn
+				di[i-1] -= dn
+			end
+			return safe(di[2:length(di)]) || safe(di[1:length(di)-1])
+		else
+			return false
+		end
+	end)
+end
+
+function day2a(f)
+	sum(broadcast(readlines(f)) do s
+		t = parse.(Int, split(s))
+		di = diff(t)
+		sg = sign.(di)
+		allequal(sg) && sg[1] != 0 && all(abs.(di) .<= 3)
+	end)
+end
+
+function main()
+	f = (length(ARGS) > 0) ? ARGS[1] : "day2/ex2.txt"
+	println("Day2a : ", day2a(f))
+	println("Day2b : ", day2b(f))
+end
+
+main()
