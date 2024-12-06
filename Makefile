aba7f472b5e4ac4c91e464052ceb3479259350f9
gmd01bit <gmd01bit@gmail.com>
2024-12-06 07:37:56 +0200

    :rocket: Day 6

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: day1 day2 day3 day4 day5
+all: day1 day2 day3 day4 day5 day6
 
 .PHONY: day1
 day1:
@@ -19,4 +19,8 @@ day4:
 .PHONY: day5
 day5:
 	julia day5/day5.jl day5/day5.txt
+
+.PHONY: day6
+day6:
+	julia day6/day6.jl day6/day6.txt
 
---
+++ day6/day6.jl
@@ -0,0 +1,60 @@
+function path(s)
+	n,m = size(s)
+	local dir0 = falses(n,m)
+	local dir1 = falses(n,m)
+	local dir2 = falses(n,m)
+	local dir3 = falses(n,m)
+	local dir = 0
+	local i = findfirst(s .== '^')
+	t = ==('#')
+	while !(i[1] in (1, n) || i[2] in (1, m))
+		if dir == 0
+			b = 1 + something(findprev(t,  s[i[1], :], i[2]), 0)
+			if dir0[i[1], b]; return nothing end
+			dir0[i[1], b:i[2]] .= true
+			i = CartesianIndex(i[1], b)
+		elseif dir == 1
+			b = -1 + something(findnext(t,  s[:, i[2]], i[1]), n+1)
+			if dir1[b, i[2]]; return nothing end
+			dir1[i[1]:b, i[2]] .= true
+			i = CartesianIndex(b, i[2])
+		elseif dir == 2
+			b = -1 + something(findnext(t,  s[i[1], :], i[2]), m+1)
+			if dir2[i[1], b]; return nothing end
+			dir2[i[1], i[2]:b] .= true
+			i = CartesianIndex(i[1], b)
+		elseif dir == 3
+			b = 1 + something(findprev(t, s[:, i[2]], i[1]), 0)
+			if dir3[b, i[2]]; return nothing end
+			dir3[b:i[1], i[2]] .= true
+			i = CartesianIndex(b, i[2])
+		end
+		dir = mod(dir + 1, 4)
+	end
+	return sum(dir0 .| dir1 .| dir2 .| dir3)
+
+end
+
+function day6b(f)
+	s = reduce(hcat, collect.(filter(!isempty, readlines(f))))
+
+	sum(broadcast(findall(s .== '.')) do u
+		s[u] = '#'
+		t = isnothing(path(s))
+		s[u] = '.'
+		t
+	end)
+end
+
+function day6a(f)
+	s = reduce(hcat, collect.(filter(!isempty, readlines(f))))
+	return path(s)
+end
+
+function main()
+	f = (length(ARGS) > 0) ? ARGS[1] : "day6/ex6.txt"
+	println("Day6a : ", day6a(f))
+	println("Day6b : ", day6b(f))
+end
+
+main()
